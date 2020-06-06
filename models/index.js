8f053978e4d4a8e352b7d9a24dc468d01a79618a
Brhymer <leiqien28@hotmail.com>
2020-06-05 17:37:52 -0700

    adds controller index, models index

---
+++ models/index.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+
+const connectionString = process.env.MONGODB_URI || "mongodb:http://localhost:27017/proj4"
+const configOptions = {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+}
+
+mongoose.connect(connectionString, configOptions)
+    .then(() => console.log('MongoDB successfully connected...'))
+    .catch(err => console.log(`MongoDB connection error: ${err}`))
+
+module.exports = {
+    Income: require('./income'),
+    Expense: require('./expense')
+}
